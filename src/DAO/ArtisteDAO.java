3218103e7619bbf7f87aad91395594ec05c565ae
Louka Gaspari <loukagaspari@hotmail.com>
2020-11-11 13:49:54 +0100

    initilisation des dao

--- src/DAO/ArtisteDAO.java
+++ src/DAO/ArtisteDAO.java
@@ -3,7 +3,6 @@ package DAO;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
 import POJO.*;
 
 
@@ -16,30 +15,66 @@ public class ArtisteDAO extends DAO<Artiste> {
 	
 	//Fonctions
 	public  boolean create(Artiste obj) {
+		
 		try {
-			
 		String ajout = "INSERT INTO Personne (Nom,Prenom,Adresse,Discriminator,Email,MotDePasse) "
 				+ "values ('" + obj.getNom() + "','" + obj.getPrenom() + "','" + obj.getAdresse() + "','Artiste','"+ obj.getEmail() + "','" + obj.getPassword() + "');";
 		System.out.println(ajout);
-		connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
-				.executeUpdate(ajout);
+		connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeUpdate(ajout);
 		}
+		
 		catch(Exception e) {
 			return false;
 		}
+		
 		return true;
 	}
 	
 	public  boolean delete(Artiste obj) {
 		
+		try {	
+			String delete = "DELETE FROM Personne where idPersonne = " + obj.getId() + ";";
+			System.out.println(delete);
+			connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeUpdate(delete);
+		}
+		
+		catch(Exception e) {
+			return false;
+		}
+		
+		return true;
 	}
 	
 	public  boolean update(Artiste obj) {
 		
+		try {	
+		String update = "UPDATE Personne set Nom = '"+ obj.getNom() + "', Prenom = '" + obj.getPrenom() + "', Adresse= '" + obj.getAdresse() + "', Email = '" + obj.getEmail() + "', MotDePasse ='" + obj.getPassword() + "');";
+		System.out.println(update);
+		connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeUpdate(update);
+		}
+		
+		catch(Exception e) {
+			return false;
+		}
+		
+		return true;
 	}
 	
 	public  Artiste find(int Id) {
+		Artiste artiste = null;
 		
+		try {
+			ResultSet result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
+					.executeQuery("SELECT * FROM Personne WHERE idPersonne = " + Id + ";");
+			if (result.first())
+				artiste = new Artiste(result.getString("Nom"), result.getString("Prenom"), result.getString("Adresse"),
+						result.getString("Email"), result.getString("MotDePasse"), result.getInt("IdPersonne"));
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return artiste;
 	}
 
 }
